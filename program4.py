de489505931f89ac43375616218abb9988fc724a
Md. Maksudul Haque <116582099+maksudrakib44@users.noreply.github.com>
2024-07-10 17:35:26 +0600

    Add files via upload

---
+++ program4.py
@@ -0,0 +1,19 @@
+x=5
+y="John"
+
+a= str(3)
+b= int(3)
+c= float(3)
+
+d,e,f = "Orange", "Banana", "Mango"
+p=q=r = "Orange"
+
+fruits = ["Mango", "Orange", "Cherry"]
+g,h,i = fruits
+
+print(type(x))
+print(type(y))
+print(a,b,c)
+print(d,e,f)
+print(p,q,r)
+print(g,h,i)
